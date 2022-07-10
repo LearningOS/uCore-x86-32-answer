2f2da19322072d988dbdee541cf2d7971fff533e
许善朴 <798304030@qq.com>
2022-07-10 23:02:56 +0800

    lab4

---
+++ labcodes_answer/lab4_result/kern/schedule/sched.c
@@ -0,0 +1,47 @@
+#include <list.h>
+#include <sync.h>
+#include <proc.h>
+#include <sched.h>
+#include <assert.h>
+
+void
+wakeup_proc(struct proc_struct *proc) {
+    assert(proc->state != PROC_ZOMBIE && proc->state != PROC_RUNNABLE);
+    proc->state = PROC_RUNNABLE;
+    cprintf("[SPOC]* now setting %d to PROC_RUNNABLE in wakeup_proc\n", proc->pid);
+}
+
+void
+schedule(void) {
+    bool intr_flag;
+    list_entry_t *le, *last;
+    struct proc_struct *next = NULL;
+    local_intr_save(intr_flag);
+    {
+        current->need_resched = 0;
+        last = (current == idleproc) ? &proc_list : &(current->list_link);
+        le = last;
+        do {
+            if ((le = list_next(le)) != &proc_list) {
+                next = le2proc(le, list_link);
+                if (next->state == PROC_RUNNABLE) {
+                    break;
+                }
+            }
+        } while (le != last);
+        if (next == NULL || next->state != PROC_RUNNABLE) {
+            next = idleproc;
+        }
+        next->runs ++;
+        if (next != current) {
+            cprintf("[SPOC] --will run %s:\n", get_proc_name(next));
+            proc_run(next);
+        }
+        else{
+            cprintf("[SPOC] --proccess remains in: %s\n", get_proc_name(current));           
+        }
+
+    }
+    local_intr_restore(intr_flag);
+}
+
